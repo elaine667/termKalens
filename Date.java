84129fb312974a29f6d0b65d63a7ce5b6fd246d5
ayu7 <ayu7@stuy.edu>
2016-06-07 19:05:05 -0400

    Add files via upload

--- Date.java
+++ Date.java
@@ -1,27 +1,27 @@
 public class Date{
 
-	private int _m, _d;
-	private Schedule _s;
+ private int _m, _d;
+ private Schedule _s;
 
-	public Date(int month, int date, Schedule schedule){
-		_m = month;
-		_d = date;
-		_s = schedule;
-	}
+ public Date(int month, int date, Schedule schedule){
+  _m = month;
+  _d = date;
+  _s = schedule;
+ }
 
-	public Date(int month, int date){
-		this(_m, _d, null);
-	}
+ public Date(int month, int date){
+  this(month, date, null);
+ }
 
-	public int getMonth(){
-		return _m;
-	}
+ public int getMonth(){
+  return _m;
+ }
 
-	public int getDate(){
-		return _d;
-	}
+ public int getDate(){
+  return _d;
+ }
 
-	public Schedule getSchedule(){
-		return _schedule;
-	}
+ public Schedule getSchedule(){
+  return _s;
+ }
 }
