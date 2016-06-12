bea78e0b944a99b3e071c85d7a50ebfae22b99f7
Julius Freyra <julius@freyra.org>
2016-06-12 18:18:18 -0400

    start of remove

--- App.java
+++ App.java
@@ -12,87 +12,92 @@ public class App{
 		String next = "";
 		boolean quit = false;
 		OVERALL : while(!next.equals("quit") && !quit){
-			quit = addEvents(next, sc, quit, calendar);
+			System.out.println("What would you like to do? 'add';'remove';'quit'");
+			next = sc.nextLine();
+			if(next.equals("add"))
+				quit = addEvents(sc, calendar);
+			if(next.equals("remove"))
+				quit = false;
+			if(next.equals("quit"))
+				quit = true;
 		}
 	}
 	
-	public static boolean addEvents(String next, Scanner sc, boolean quit, PriorityQueue<Date> calendar){
-			int j,s,st,en;
-			j = s = st = en = 0;
-			DATE : for(;;){
-				System.out.println("Enter the number corresponding to a date within this year (2016); enter cancel to cancel entry");
-				System.out.println("i.e. 06/12 for June 12th, 12/01 for December 1st: ");
-				next = sc.nextLine();
-				if(next.equals("cancel")){
-					System.out.println("Entry cancelled"); // cancellation
-					System.out.println();
-					return false;
-				}
-				if(next.equals("quit"))
-					return true;
-				if(next.length() == 5){
-					String a = next.substring(0,2);
-					String b = next.substring(3,5);
-					if(isInteger(a) && isInteger(b)){
-						j = Integer.parseInt(a); // month
-						s = Integer.parseInt(b); // day
-						break;
-					}
-					else{
-						System.out.println("Please enter a valid mm/dd combination");
-						System.out.println();
-						continue DATE; // allows us to just skip the rest of the code and start from the beginning
-					}
-					// m and n have no leading zeroes
+	public static boolean removeEvents(Scanner sc, PriorityQueue<Date> calendar){
+		return false;
+	}
+	
+	public static boolean addEvents(Scanner sc, PriorityQueue<Date> calendar){
+		int j,s,st,en;
+		j = s = st = en = 0;
+		DATE : for(;;){
+			System.out.println("Enter the number corresponding to a date within this year (2016); enter cancel to cancel entry");
+			System.out.println("i.e. 06/12 for June 12th, 12/01 for December 1st: ");
+			String next = sc.nextLine();
+			if(next.equals("cancel")){
+				System.out.println("Entry cancelled"); // cancellation
+				System.out.println();
+				return false;
+			}
+			if(next.equals("quit"))
+				return true;
+			if(next.length() == 5){
+				String a = next.substring(0,2);
+				String b = next.substring(3,5);
+				if(isInteger(a) && isInteger(b)){
+					j = Integer.parseInt(a); // month
+					s = Integer.parseInt(b); // day
+					break;
 				}
 				else{
 					System.out.println("Please enter a valid mm/dd combination");
 					System.out.println();
-					continue DATE;
+					continue DATE; // allows us to just skip the rest of the code and start from the beginning
 				}
+				// m and n have no leading zeroes
 			}
-			System.out.println("Enter the title of your event; enter cancel to cancel entry");
-			String title = sc.nextLine();
-			if(title.equals("cancel")){
+			else{
+				System.out.println("Please enter a valid mm/dd combination");
+				System.out.println();
+				continue DATE;
+			}
+		}
+		System.out.println("Enter the title of your event; enter cancel to cancel entry");
+		String title = sc.nextLine();
+		if(title.equals("cancel")){
+			System.out.println("Entry cancelled"); // cancellation
+			System.out.println();
+			return false;
+		}
+		if(title.equals("quit"))
+			return true;
+		System.out.println("Enter the description of your event; enter cancel to cancel entry");
+		String desc = sc.nextLine();
+		if(desc.equals("cancel")){
+			System.out.println("Entry cancelled"); // cancellation
+			System.out.println();
+			return false;
+		}
+		if(desc.equals("quit"))
+			return true;
+		HOURS : for(;;){
+			System.out.println("Enter the start and end hours (24hr time) of your event; enter cancel to cancel entry");
+			System.out.println("i.e. 06/12 for 6am to 12pm, 14/18 for 2pm to 6pm: ");
+			String times = sc.nextLine();
+			if(times.equals("cancel")){
 				System.out.println("Entry cancelled"); // cancellation
 				System.out.println();
 				return false;
 			}
-			if(title.equals("quit"))
+			if(times.equals("quit"))
 				return true;
-			System.out.println("Enter the description of your event; enter cancel to cancel entry");
-			String desc = sc.nextLine();
-			if(desc.equals("cancel")){
-				System.out.println("Entry cancelled"); // cancellation
-				System.out.println();
-				return false;
-			}
-			if(desc.equals("quit"))
-				return true;
-			HOURS : for(;;){
-				System.out.println("Enter the start and end hours (24hr time) of your event; enter cancel to cancel entry");
-				System.out.println("i.e. 06/12 for 6am to 12pm, 14/18 for 2pm to 6pm: ");
-				String times = sc.nextLine();
-				if(times.equals("cancel")){
-					System.out.println("Entry cancelled"); // cancellation
-					System.out.println();
-					return false;
-				}
-				if(times.equals("quit"))
-					return true;
-				if(times.length() == 5){
-					String start = times.substring(0,2);
-					String end = times.substring(3,5);
-					if(isInteger(start) && isInteger(end)){
-						st = Integer.parseInt(start); // start
-						en = Integer.parseInt(end); // end
-						break;
-					}
-					else{
-						System.out.println("Please enter a valid ss/ee combination");
-						System.out.println();
-						continue HOURS;
-					}
+			if(times.length() == 5){
+				String start = times.substring(0,2);
+				String end = times.substring(3,5);
+				if(isInteger(start) && isInteger(end)){
+					st = Integer.parseInt(start); // start
+					en = Integer.parseInt(end); // end
+					break;
 				}
 				else{
 					System.out.println("Please enter a valid ss/ee combination");
@@ -100,39 +105,45 @@ public class App{
 					continue HOURS;
 				}
 			}
-			Event event;
-			PRIORITY : for(;;){
-				System.out.println("What is your event priority? 1-5 5 being the least important");
-				String pri = sc.nextLine();
-				if(pri.equals("cancel")){
-					System.out.println("Entry cancelled"); // cancellation
-					System.out.println();
-					return false;
-				}
-				if(pri.equals("quit"))
-					return true;
-				boolean newEvent = false;
-				if(pri.length() == 1 && isInteger(pri)){
-					event = new Event(title,desc,j,s,0,0,Integer.parseInt(pri));
-					newEvent = true;
-					break;
-				}
-				else{
-					System.out.println("Please enter a valid priority");
-					System.out.println();
-					continue PRIORITY;
-				}
+			else{
+				System.out.println("Please enter a valid ss/ee combination");
+				System.out.println();
+				continue HOURS;
 			}
-			Date temp = new Date(j,s,new Schedule());
-			boolean conflict = false;
-			for(Date e : calendar){
-				if(e.equals(temp)){
-					conflict = !e.addEvent(event); // addEvent should return a boolean on whether or not the event can be added
-				}
+		}
+		Event event;
+		PRIORITY : for(;;){
+			System.out.println("What is your event priority? 1-5 5 being the least important");
+			String pri = sc.nextLine();
+			if(pri.equals("cancel")){
+				System.out.println("Entry cancelled"); // cancellation
+				System.out.println();
+				return false;
 			}
-			if(conflict)
-				calendar.offer(temp);
-			return false;
+			if(pri.equals("quit"))
+				return true;
+			boolean newEvent = false;
+			if(pri.length() == 1 && isInteger(pri)){
+				event = new Event(title,desc,j,s,0,0,Integer.parseInt(pri));
+				newEvent = true;
+				break;
+			}
+			else{
+				System.out.println("Please enter a valid priority");
+				System.out.println();
+				continue PRIORITY;
+			}
+		}
+		Date temp = new Date(j,s,new Schedule());
+		boolean conflict = false;
+		for(Date e : calendar){
+			if(e.equals(temp)){
+				conflict = !e.addEvent(event); // addEvent should return a boolean on whether or not the event can be added
+			}
+		}
+		if(conflict)
+			calendar.offer(temp);
+		return false;
 	}
 	
 	public static boolean isInteger(String a) {
