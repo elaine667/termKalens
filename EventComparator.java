db8ab6cdad402a13e215d98a0357c5b088fe19c0
kpoon17 <kpoon1@stuy.edu>
2016-06-12 18:14:22 -0400

    Update EventComparator.java

--- EventComparator.java
+++ EventComparator.java
@@ -5,18 +5,17 @@ public class EventComparator<E> implements Comparator<E>{
 
 	// negative is higher priority
     public int compare(E lhs, E rhs) throws ClassCastException{
-      Event a=(Event)lhs;
-      Event b=(Event)rhs;
-      if(a.getMonth() != b.getMonth())
-        return b.getMonth() - a.getMonth();
-      else if(a.getDay() != b.getDay())
-        return b.getDay() - a.getDay();
-      else if(a.getPriority() != b.getPriority())
-        return b.getPriority() - a.getPriority();
-      else if(a.getStart() != b.getStart())
-        return b.getStart() - a.getStart();
-      else if(a.getEnd() != b.getEnd())
-        return b.getEnd() - a.getEnd();
-      return 0;
+	Event a=(Event)lhs;
+	Event b=(Event)rhs;
+	if(a.getMonth() != b.getMonth()){ //lower # months first
+	    return (a.getMonth()- b.getMonth()); 
+	else if(a.getDay() != b.getDay()) //lower days first
+	    return a.getDay() - b.getDay();
+	else if(a.getPriority() != b.getPriority()) //lower # means higher priority
+	    return a.getPriority() - b.getPriority();
+	else if (a.getEnd()> b.getStart() || b.getEnd() > a.getStart()) //if times over lap
+	    return -100; //indicating a conflict
+	return 0;
     }
 }
+
