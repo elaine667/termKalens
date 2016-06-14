d86c417de122a4029f17f634756c6ef17c090e3d
kpoon17 <kpoon1@stuy.edu>
2016-06-13 22:49:29 -0400

    Add files via upload

--- Date.java
+++ Date.java
@@ -26,16 +26,23 @@ public class Date{
  }
  
  public boolean addEvent(Event e){
-	 return _s.add(e);
+  return _s.add(e);
  }
  
  public boolean equals(Object o){
-	 Date d = (Date) o;
-	 return getMonth() == d.getMonth() && getDate() == d.getDate();
+  Date d = (Date) o;
+  return getMonth() == d.getMonth() && getDate() == d.getDate();
  }
  
  public String toString(){
-	 return getMonth()+"/"+getDate();
+   
+  return getMonth()+"/"+getDate() + "\n" + _s +"\n--------";
  }
- 
+ public static void main(String[] args){
+   Schedule x = new Schedule();
+   Event e = new Event ("a","a",1,1,1,1,1);
+   x.add(e);
+   Date a = new Date(12, 23,x);
+   System.out.println(a);
+ }
 }
